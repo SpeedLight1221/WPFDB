9f071a56d4bf1911a35bfaf911eec2024e0c9b2b
Vojtěch Charouz <vojtacharouz@gmail.com>
2024-02-20 12:02:40 +0100

    Přidejte soubory projektu.

---
+++ db/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace db
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        const string dbFile = "C:\\Temp/char/data.db";
+        const string tableName = "MyTable";
+        const string connectionString = "Data Source=" + dbFile + ";Version=3";
+        public MainWindow()
+        {
+            InitializeComponent();
+            inntDB();
+        }
+
+        private void inntDB()
+        {
+            using(var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                var command = new SQLiteCommand($"CREATE TABLE IF NOT EXISTS {tableName} " + $"(Id INTEGER PRIMARY KEY,name TEXT)",conn);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void BtAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
